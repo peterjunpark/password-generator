8f1cc76cd9b5be1f01e2c3a00d7ffffd007209e3
Peter Park <peterjpark@pm.me>
2023-03-22 02:10:55 -0400

    Add getCharSets function and validation logic

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ var generateBtn = document.querySelector("#generate");
 var passwordCriteria = {
   numOfChars: 16,
   chars: {
-    "lowercase letters": "abcdefghijklmnopqrstuvwxyz",
-    "uppercase letters": "ABCDEFGHIJIKLMNOPQRSTUVWXYZ",
-    numbers: "0123456789",
-    "special characters": " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+    "lowercase letters": "",
+    "uppercase letters": "",
+    numbers: "",
+    "special characters": "",
   },
 
   getNumOfChars: function () {
     var desiredLength = prompt(
-      "Enter the length of your generated password (must contain at least 8 and no more than 128 characters).",
+      "Enter the length of your generated password.\n\nYour password must contain at least 8 and no more than 128 characters.",
       this.numOfChars
     );
-    if (desiredLength > 8 && desiredLength < 128) {
+    if (desiredLength >= 8 && desiredLength <= 128) {
       this.numOfChars = desiredLength;
-      this.getChars();
-    } else {
+      this.getCharSets();
+    } else if (desiredLength !== null) {
       alert(
         "Your generated password must contain at least 8 and no more than 128 characters.\n\nEnter your desired password length."
       );
@@ -26,30 +26,59 @@ var passwordCriteria = {
     }
   },
 
-  getChars: function () {
-    for (var key in this.chars) {
-      if (!confirm(`Would you like your generated password to include ${key}?\n\nSelect 'OK' to include them or 'Cancel' to exclude them.`)) {
-        this.chars[key] = ""
+  getCharSets: function () {
+    //Assign charsets here to add back the string of characters if user clicks "Generate Password" without refreshing the page
+    this.chars["lowercase letters"] = "abcdefghijklmnopqrstuvwxyz";
+    this.chars["uppercase letters"] = "ABCDEFGHIJIKLMNOPQRSTUVWXYZ";
+    this.chars["numbers"] = "0123456789";
+    this.chars["special characters"] = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+    if (
+      confirm(
+        "Select which character sets to include in your generate password using the following prompts."
+      )
+    ) {
+      for (var charset in this.chars) {
+        if (
+          !confirm(
+            `Would you like your generated password to include ${charset}?\n\nSelect 'OK' to include them or 'Cancel' to exclude them.`
+          )
+        ) {
+          this.chars[charset] = "";
+        }
       }
+      this.validateCriteria();
+    } else {
+      this.getNumOfChars();
     }
-    this.printCriteria();
   },
 
-  printCriteria: function () {
-    var desiredCriteria = `Your generated password will include ${this.numOfChars} characters and include the following character sets:\n`;
-    for (var key in this.chars) {
-      if (this.chars[key] !== "") {
-        desiredCriteria += "\n · " + key.charAt(0).toUpperCase() + key.slice(1); // Capitalize first letter when displaying name of character set
-      };
+  validateCriteria: function () {
+    var desiredCriteria = "";
+    for (var charset in this.chars) {
+      if (this.chars[charset]) {
+        // Checks that at least 1 character set is selected
+        desiredCriteria +=
+          "\n · " + charset.charAt(0).toUpperCase() + charset.slice(1); // Capitalize first letter when displaying name of character set
+      }
     }
-    if (!confirm(desiredCriteria)) this.getNumOfChars();
-
+    if (!desiredCriteria) {
+      alert("You must select at least 1 character set.\n\nPlease try again.");
+      this.getChars();
+    } else if (!desiredCriteria) this.getChars();
+    else {
+      alert(
+        `Your generated password will include ${this.numOfChars} characters and include the following character sets:\n${desiredCriteria}`
+      );
+    }
     console.log(this.chars);
   },
 };
 
 function generatePassword() {
   passwordCriteria.getNumOfChars();
+  
+
 }
 
 // Write password to the #password input
